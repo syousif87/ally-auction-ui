80dabf6429f2300eab8ffdae5dc0d71507ef0e08
Samer Yousif <samerpyousif@gmail.com>
2020-03-05 14:14:04 -0500

    Fixed e2e test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ally-auction-ui app is running!');
+    expect(page.getTitleText()).toEqual('Ally Auction Demo');
   });
 
   afterEach(async () => {
